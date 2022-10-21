12b1d6a4d721996820512895cca914bfdb38b310
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-10-21 06:38:09 +0100

    Imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import { Link } from 'react-router-dom';
+
 import './App.css';
+import profilePicture from '../public/assets/profile__img.png';
+import share from '../public/assets/_Avatar share button.png';
+import option from '../public/assets/mobile_Avatar share button.png';
+import github from '../public/assets/github-icon.png';
+import I4G from '../public/assets/I4G.png';
+import zuri from '../public/assets/logo.36d2d48a.png';
 
 function App() {
   return <></>;
