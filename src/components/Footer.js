8fd7266744fb998a324fa49f91eb4f9f4a0f7bec
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-11-03 11:03:21 +0100

    feat: add footer jsx

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import I4G from '../assets/I4G.png';
+import zuri from '../assets/logo.36d2d48a.png';
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={zuri} alt='zuri logo' />
+      <small>HNG Internship 9 Frontend Task</small>
+      <img src={I4G} alt='I4G logo' />
+    </footer>
+  );
+};
+
+export default Footer;
